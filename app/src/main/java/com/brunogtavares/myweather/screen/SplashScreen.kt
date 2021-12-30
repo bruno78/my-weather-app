96b9d1085df917c1a4d0b5c1ebd936654d877783
Bruno G Tavares <bgtavares@Brunos-MBP.lan>
2021-12-30 13:05:06 -0500

    Add Splash screen added resources for splash screen, add navigation to Main screen

--- app/src/main/java/com/brunogtavares/myweather/screen/SplashScreen.kt
+++ app/src/main/java/com/brunogtavares/myweather/screen/SplashScreen.kt
@@ -1,11 +1,82 @@
 package com.brunogtavares.myweather.screen
 
-import android.window.SplashScreen
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.brunogtavares.myweather.R
+import com.brunogtavares.myweather.navigation.MyWeatherScreens
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    Text("SplashScreen")
+    val scale = remember {
+        Animatable(initialValue = 0f)
+    }
+
+    LaunchedEffect(key1 = true, block = {
+        scale.animateTo(
+            targetValue = 0.9f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(8f)
+                        .getInterpolation(it)
+                }
+            )
+        )
+        delay(2000L) // delay the screen for 2 seconds for now
+        navController.navigate(route = MyWeatherScreens.MainScreen.name)
+    })
+
+    Surface(
+        modifier = Modifier
+            .padding(16.dp)
+            .size(330.dp)
+            .scale(scale.value),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(
+            width = 2.dp,
+            color = Color.LightGray
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                modifier = Modifier.size(95.dp),
+                painter = painterResource(R.drawable.sun),
+                contentScale = ContentScale.Fit,
+                contentDescription = "Sunny icon splash screen image"
+            )
+            Text(
+                text = "Find the Sun?",
+                style = MaterialTheme.typography.h5,
+                color = Color.LightGray
+            )
+        }
+    }
 }
