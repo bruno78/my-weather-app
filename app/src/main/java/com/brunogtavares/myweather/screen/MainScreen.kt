22ea6cc73d7f10dbd89f72eaf3de451c29a7738c
Bruno G Tavares <bgtavares@Brunos-MBP.lan>
2021-12-30 16:48:34 -0500

    Add appbar, created widgets folder

--- app/src/main/java/com/brunogtavares/myweather/screen/MainScreen.kt
+++ app/src/main/java/com/brunogtavares/myweather/screen/MainScreen.kt
@@ -1,23 +1,54 @@
 package com.brunogtavares.myweather.screen
 
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.brunogtavares.myweather.model.WeatherLocation
 import com.brunogtavares.myweather.viewmodel.MainScreenState
 import com.brunogtavares.myweather.viewmodel.MainViewModel
+import com.brunogtavares.myweather.widgets.AppBar
 
 @Composable
 fun MainScreen(
     navController: NavController,
     viewModel: MainViewModel
-){
+) {
     viewModel.onLoadWeather("New York")
 
-    when(viewModel.screenState.value) {
+    when (viewModel.screenState.value) {
         is MainScreenState.Loading -> CircularProgressIndicator()
-        is MainScreenState.LoadSuccess -> Text("Main Screen")
+        is MainScreenState.LoadSuccess -> MainScaffold(
+            weatherLocation = (viewModel.screenState.value as MainScreenState.LoadSuccess).data,
+            navController = navController
+        )
         is MainScreenState.LoadFailed -> Text("Failed")
     }
 
+
+}
+
+@Composable
+fun MainScaffold(
+    weatherLocation: WeatherLocation,
+    navController: NavController
+) {
+    Scaffold(
+        topBar = {
+            AppBar(
+                title = weatherLocation.city.name + " ,${weatherLocation.city.country}",
+                navController = navController,
+                elevation = 5.dp
+            )
+        }
+    ) {
+        MainContent(weatherLocation)
+    }
+}
+
+@Composable
+fun MainContent(weatherLocation: WeatherLocation) {
+    Text(weatherLocation.city.name)
 }
---
+++ app/src/main/java/com/brunogtavares/myweather/widgets/AppBar.kt
@@ -0,0 +1,77 @@
+package com.brunogtavares.myweather.widgets
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Preview
+@Composable
+fun AppBar(
+    title: String = "Siarou",
+    icon: ImageVector? = null,
+    isMainScreen: Boolean = true,
+    elevation: Dp = 0.dp,
+    navController: NavController,
+    onAddActionClicked: () -> Unit = {},
+    onButtonClicked: () -> Unit = {}
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                color = MaterialTheme.colors.onSecondary,
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp
+                ),
+            )
+        },
+        actions = {
+            if (isMainScreen) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search Icon"
+                    )
+                }
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Rounded.MoreVert,
+                        contentDescription = "Search Icon"
+                    )
+                }
+            } else {
+                Box {}
+            }
+
+        },
+        navigationIcon = {
+            icon?.let {
+                Icon(
+                    imageVector = it,
+                    contentDescription = "Navigate up",
+                    tint = MaterialTheme.colors.onSecondary,
+                    modifier = Modifier.clickable {
+                        onButtonClicked()
+                    }
+                )
+            }
+        },
+        backgroundColor = Color.Transparent,
+        elevation = elevation
+    )
+}
