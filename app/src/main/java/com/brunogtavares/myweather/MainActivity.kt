7fd7a9b4c49b706bff5ecaa131af505f88b2aef8
Bruno G Tavares <bgtavares@Brunos-MBP.lan>
2022-01-01 19:38:54 -0500

    add about screen implemented about screen

--- app/src/main/java/com/brunogtavares/myweather/MainActivity.kt
+++ app/src/main/java/com/brunogtavares/myweather/MainActivity.kt
@@ -8,15 +8,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.brunogtavares.myweather.navigation.MyWeatherNavigation
 import com.brunogtavares.myweather.ui.theme.MyWeatherTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalComposeUiApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +28,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@ExperimentalComposeUiApi
 @Composable
 fun MyWeatherApp() {
     MyWeatherTheme {
@@ -45,6 +47,7 @@ fun MyWeatherApp() {
     }
 }
 
+@ExperimentalComposeUiApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
